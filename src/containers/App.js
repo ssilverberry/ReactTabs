87243b0f9b852d17867212dc8306aa82a2783f59
Paul <pashenkagor@gmail.com>
2019-01-28 01:09:35 +0200

    implemented withAccordion hoc

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 // tab's components
 import TabHead from 'components/tabs/header/TabHead'
 import ImgContent from 'components/tabs/content/ImgContent'
@@ -17,46 +17,28 @@ import { titles, content } from 'constants/tables'
 import { TabNav, Wrapper } from 'styles/tabs'
 // tabs container
 import Tabs from 'containers/Tabs'
+// hoc(s)
+import withAccordion from 'hoc/withAccordion'
 
-class App extends Component {
-  state = {
-    activeAccordion: ''
-  }
-
-  handleAccordionClick = activeAccordion => {
-    this.setState(state => ({
-      activeAccordion: state.activeAccordion !== activeAccordion ? activeAccordion : ''
-    }))
-  }
-
-  render() {
-    const { activeAccordion } = this.state
-
-    return (
-      <Wrapper>
-        <Tabs defaultTab={'FIRST'} defaultTabData={tabData}>
-          {({ tabData, handleActive, active }) => (
-            <Fragment>
-              <TabNav>
-                <TabHead handleActive={handleActive} active={active} tabData={tabData} />
-              </TabNav>
-              {tabsMap.FIRST === active && (
-                <AcrdContainer>
-                  <AccordionHeader
-                    accordionData={acrdnsData}
-                    handleActive={this.handleAccordionClick}
-                    active={activeAccordion}
-                  />
-                </AcrdContainer>
-              )}
-              {tabsMap.SECOND === active && <Table titles={titles} content={content} />}
-              {tabsMap.THIRD === active && <ImgContent />}
-            </Fragment>
+const App = ({ handleActiveAcc, activeAcc }) => (
+  <Wrapper>
+    <Tabs defaultTab={'FIRST'} defaultTabData={tabData}>
+      {({ tabData, handleActive, active }) => (
+        <Fragment>
+          <TabNav>
+            <TabHead handleActive={handleActive} active={active} tabData={tabData} />
+          </TabNav>
+          {tabsMap.FIRST === active && (
+            <AcrdContainer>
+              <AccordionHeader accordionData={acrdnsData} handleActive={handleActiveAcc} active={activeAcc} />
+            </AcrdContainer>
           )}
-        </Tabs>
-      </Wrapper>
-    )
-  }
-}
+          {tabsMap.SECOND === active && <Table titles={titles} content={content} />}
+          {tabsMap.THIRD === active && <ImgContent />}
+        </Fragment>
+      )}
+    </Tabs>
+  </Wrapper>
+)
 
-export default App
+export default withAccordion(App)
--- src/hoc/withAccordion.js
+++ src/hoc/withAccordion.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
 const withAccordion = WrappedComponent => {
-  return class EnhancedComponent extends React.Component {}
+  return class EnhancedComponent extends React.Component {
+    state = {
+      activeAccordion: ''
+    }
+
+    handleAccordionClick = activeAccordion => {
+      this.setState(state => ({
+        activeAccordion: state.activeAccordion !== activeAccordion ? activeAccordion : ''
+      }))
+    }
+
+    render() {
+      const { activeAccordion } = this.state
+      return (
+        <WrappedComponent {...this.props} handleActiveAcc={this.handleAccordionClick} activeAcc={activeAccordion} />
+      )
+    }
+  }
 }
 
 export default withAccordion
